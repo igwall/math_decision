554ade0f368998800d2d9459e58514b7c3b8f3b7
igwall <lucas.pro@mail.com>
2019-02-23 11:36:52 +0100

    update(fileopener) add(solver.py) update(readme)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # math_decision
 Projet pour l'algo de math de la décision
+
+## file_opener.py:
+> Ce fichier est là pour transformer les données brutes du CSV grâce à la fonction raw_traitment(data_raw)
+
+#### 1. raw_traitment(data_raw)
+##### La fonction retourne deux éléments
+- students_number (tableau) qui prends l'ensemble des valeurs de nos numéros étudiants.
+- marks qui est un tableau à deux dimensions qui va retourner seulement la matrice des notes sans prendre en compte les numéros étudiants.  
+
+## Solver.py:
+ 
--- file_opener.py
+++ file_opener.py
@@ -1,44 +1,19 @@
+#encoding: UTF-8
 import csv
-
-f1 = open("preferences.csv","r")
-lignes = f1.readlines()
-
-for ligne in lignes[1:]:
-  ligne = ligne.split(',')[1:]
-  print(ligne)
-
-f1.close()
-
-
 
 #  Traitment function of the CSV file
 #  return a list of values (marks) and a list of students numbers
 def raw_traitment(data_raw):
-    next(data_raw)  # skip header
-    student_numbers = []
-    marks = []
 
-    lignes = data_raw.readlines()
-    for ligne in lignes[1:]:
+    marks = []
+    students_numbers = []
+
+    for ligne in data_raw[1:]:
         ligne = ligne.split(',')[1:]
         marks.append(ligne)
 
+    students_numbers.append(data_raw[0])
 
-        # TODO: Retirer l'ensemble des numéros étudiants sur la ligne
-
-
-    return marks, student_numbers
-
-
-# Main
-
-print("Ouverture du fichier de préférences")
-
-
-file_preferences = open('preferences.csv', 'r')  # open csv file on file_preferences
-
-data_raw = csv.reader(file_preferences)  # data_raw va get all the values from csv file
-
-mark, students = raw_traitment(data_raw)  # Send the data to the traitment function to clean them.
-
-print("La premiere note est: " + mark[0][0])
+    for num in data_raw:
+        students_number
+    return marks, students_numbers
