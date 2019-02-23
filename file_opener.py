d94bc80b53f901bb472ad70bc420aebc14355e17
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2019-02-23 13:41:29 +0100

    delete raw_treatment method

--- file_opener.py
+++ file_opener.py
@@ -1,23 +1,6 @@
 #encoding: UTF-8
 import csv
 
-#  Traitment function of the CSV file
-#  return a list of values (marks) and a list of students numbers
-def raw_traitment(data_raw):
-
-    marks = []
-    students_numbers = []
-
-    for ligne in data_raw[1:]:
-        ligne = ligne.split(',')[1:]
-        marks.append(ligne)
-
-    students_numbers.append(data_raw[0])
-
-    for num in data_raw:
-        students_number
-    return marks, students_numbers
-
 #Method which permits to return a matrix of marks
 def extractMarks(data_raw):
     marks = []
