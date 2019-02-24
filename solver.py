3c3b281283103d565d6aa6aac417018596cd51e0
igwall <lucas.pro@mail.com>
2019-02-24 21:13:03 +0100

    implementing solver treatment

--- solver.py
+++ solver.py
@@ -15,21 +15,8 @@ def upgrade(level, marks): # enleve toutes les aretes du niveau donné en param�
                 marks[i][j] = '-1'
     return marks
 
-def traitement(index_level, marks):
-    old_marks = marks
-    upgrade(levels[index_level])
-    # Si un sommet inférieur ou égal à deux aretes:
-
-    # Si le nb arete = 2 (on a un groupe de 3)
-        #Soit on fait le groupe de trois
-        # Soit on fait ab
-        # Soit on fait ac
-
-    # Si nb arete =1
-        # On fait un groupe de 2
-
-def formerGroupe(groupe, marks): # Delete all nodes from the list of students putted in entry
-    for eleve in groupe:  # Pour chaque élève du groupe que l'on souhaite former
+def createGroup(group, marks): # Delete all nodes from the list of students putted in entry
+    for eleve in group:  # Pour chaque élève du group que l'on souhaite former
         marks = deleteNode(eleve,marks)
     return marks
 
@@ -92,16 +79,121 @@ def median(listGroup, marks):
     return levels [ medians[ round(len(medians) / 2) ] ]
 
 
-#Method which returns which group of the 2 is the best (by using the median)
-def bestGroup(listGroup1, listGroup2, marks):
+#Method which returns which group of the list is the best (by using the median)
+def bestGroup(groups, marks):
     levels = ['AR','I','P','AB','B','TB']
+    medians = []
 
-    medianValue1 = levels.index( median(listGroup1, marks) )
-    medianValue2 = levels.index( median(listGroup2, marks) )
-    if (medianValue1 > medianValue2):
-        return listGroup1
+    for i in range(len(groups)):
+        medians.append( levels.index(median(groups[i], marks)) )
+
+    maxi = max(medians)
+    index = medians.index(maxi)
+
+    return groups[index]
+
+
+#Method which permits to return the incoming degree of a vertex ( = student)
+def incomingDegree(student, marks):
+    degree = 0
+    columnStudent = []
+    for ligne in marks:
+        columnStudent.append(ligne[student])
+
+    for i in range( 0, len(columnStudent) ):
+        if columnStudent[i] != '-1':
+            degree = degree + 1
+    return degree
+
+def minEqualTwoDegree(marks):
+    i = 0
+    studentDegLessEqTwo = False
+    while i < len(marks) and !(studentDegLessEqTwo):
+        if incomingDegree(i, marks) <= 2:
+            studentDegLessEqTwo = True
+        i = i + 1
+    return studentDegLessEqTwo
+
+
+
+def studentAvailable(marks):
+    available = False
+    for student in marks:
+        for elem in student:
+            if elem != '-1':
+                available = True
+    return available
+
+#Method which permits to return the student with the degree given in parameters and the students linked to this student
+def groups(marks, degree):
+    i = 0
+    students = []
+
+    for i in range(len(marks)):
+        if incomingDegree(i, marks) == degree:
+            stu =[]
+            stu.append(i)
+            for j in range(len(marks[i])):
+                if marks[i][j] != -1:
+                    stu.append(j)
+            students.append(stu)
+        return students
+
+# Method which try to find intersections between different groups
+def intersection(listOfGroups):
+    elements = []
+    groupsIntersect
+    for group in listOfGroups:
+        for student in group:
+            if student in elements:
+
+
+
+# levels = ['AR','I','P','AB','B','TB']
+def treatment(marks, levels, index_level):
+
+    #si tous les sommets ont un degré entrant = 0 : finish return la liste des groupes
+    available = studentAvailable(marks)
+    if available == False:
+        return []
     else:
-        return listGroup2
+        #supprimer les arêtes de plus bas en gardant en mémoire la matrice avant la suppression
+        marksUpgraded = upgrade(index_level, marks)
+
+        #soit personne n'a de degré entrant <= 2 -> on relance l'algo sur niveau plus haut
+        if !(minEqualTwoDegree(marks)):
+            return treatment(marksUpgraded, levels, index_level+1)
+
+        #soit une personne a un degré entrant = 1 -> on crée le groupe et on relance l'algo sur niveau supérieur
+        binoms = groups(marks, 1)
+        if len(binoms) != 0:        # On a qu'un seul binome
+
+        intersectedGroups = intersect(binoms) #on récupère tous les binomes en intersection
+        for group in intersectedGroups:
+
+            #Modifier en enlevant les sommets
+            marksUpgraded = createGroup(binoms[0], marksUpgraded)
+            return binoms[0] + treatment(marksUpgraded, levels, index_level)
+        #else:         # On a plusieurs binomes et l'affectation d'un binome peut influencer un autre binome. On va descendre en profondeur.
+            #Vérifier si il n'y a pas d'intersection entre les éléments qui se pointent
+            #On créer les binomes sur ceux qui ne s'intersectent pas
+
+        trinoms = groups(marks, 2)
+        if len(trinoms) != 0:
+
+
+
+        #soit une personne a un degré entrant = 2 -->
+            #AB = traitment quand je fais AB
+            #AC = traitment quand je fais AC
+            # result = bestGroup(AB,AC) bestGroup entre le trinome et les 2 binomes possibles
+
+        #soit une personne a un degré entrant = 0
+        bitrinoms = groups(marks, 0)
+        if len(bitrinoms) != 0:
+
+
+
 
 
 
@@ -113,6 +205,7 @@ file_preferences = open('preferences.csv', 'r')  # open csv file on file_prefere
 data_raw = file_preferences.readlines()  # data_raw va get all the values from csv file
 marks = extractMarks(data_raw)
 marks = initialize(marks)
+#qgis
 students = extractStudentsNumbers(data_raw)
 file_preferences.close()
 
